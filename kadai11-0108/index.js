d3f57cc7319798ca90205aba8bf94411d9704282
oga-369 <lt1026he@gmail.com>
2020-01-15 05:21:25 +0000

    課題

--- kadai11-0108/index.js
+++ kadai11-0108/index.js
@@ -1,10 +1,22 @@
 const http = require('http');
+
 const server = http.createServer((req, res) => {
-    // CORS ヘッダーを返す
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.write('hello');
-    res.end();
-    });
+        let result
+        const num = Math.random()
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+            if(num < 0.3) {
+                res.write('黄色のマフラー')
+            } else　if(num < 0.5) {
+                res.write('スルメ')
+            } else if(num < 0.8) {
+                res.write('マシュマロ')
+            } else {
+                res.write('赤い靴下')
+            }
+    
+    res.end()
+    })
     server.listen(4000);
     console.log('Listening port 4000...');
 
